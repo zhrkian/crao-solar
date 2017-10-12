cdfcca84da07e4f563eb526b4d1eb12440431749
R.Zhygalkin <r.zhygalkin@gmail.com>
2017-10-12 12:47:03 +0300

    remove backend-dist

--- backend-dist/routes/api.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.use('/test', require('./api_test'))
-router.use('/jobs', require('./api_jobs'))
-
-module.exports = router
--- backend-dist/routes/api_tesos.js
+++
@@ -1,9 +0,0 @@
-const express               = require('express')
-const router                = express.Router()
-const wrap                  = require('co-express')
-
-router.get('/', wrap(function *(req, res) {
-  return res.json({ success: true })
-}))
-
-module.exports = router
