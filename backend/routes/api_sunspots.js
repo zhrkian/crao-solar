f24e1e67805ed56b562da2765e3a044442ba6dd9
R.Zhygalkin <r.zhygalkin@gmail.com>
2017-10-31 00:37:59 +0300

    Deploy

--- backend/routes/api_sunspots.js
+++ backend/routes/api_sunspots.js
@@ -1,9 +1,11 @@
 const express               = require('express')
 const router                = express.Router()
 const wrap                  = require('co-express')
+const jobs                  = require('../modules/job')
 
 router.get('/', wrap(function *(req, res) {
-  return res.json({ success: true })
+  const list = yield jobs.index()
+  return res.json({ success: true, jobs: list })
 }))
 
 module.exports = router
